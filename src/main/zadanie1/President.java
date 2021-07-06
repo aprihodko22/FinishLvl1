85a8a6a1278bd2284298db4dab8500aa730d795f
aprihodko22 <dotaroad1kmmr@mail.ru>
2021-07-06 21:13:45 +0300

    add new law

--- src/main/zadanie1/President.java
+++ src/main/zadanie1/President.java
@@ -2,6 +2,6 @@ package zadanie1;
 
 public class President {
     public static void main(String[] args) {
-        System.out.println("I like to rule");
+        System.out.println("new law: IT professionals do not pay taxes");
     }
 }
